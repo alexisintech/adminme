804ffe92bf77700bb31007da33590c179340d60d
Alexis Aguilar <alexmerona@gmail.com>
2023-02-08 11:53:04 -0500

    updating react pro sidebar dependency,  creating sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { Routes, Route } from "react-router-dom";
 import { ColorModeContext, useMode } from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Appbar from "./scenes/global/Appbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -10,8 +13,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline>
           <div className="app">
+            <Sidebar />
             <main className="content">
               <Appbar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+              </Routes>
             </main>
           </div>
         </CssBaseline>
